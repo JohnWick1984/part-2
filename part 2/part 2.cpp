eafa57bc10916a0892090148d86e9e88a08955df
vboxuser <vboxuser@WINDA10>
2023-02-09 18:46:16 +0500

    Добавьте файлы проекта.

---
+++ part 2/part 2.cpp
@@ -0,0 +1,42 @@
+﻿// part 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+
+    int min, max, begin, finish;
+    int money[12];
+
+
+    for (int i = 0; i < 12; i++)
+    {
+        cout << "Введите доход за " << i + 1 << "  месяц:\n";
+        cin >> money[i];
+    }
+
+    cout << "Введите месяц начала периода : ";
+    cin >> begin;
+    cout << "Введите месяц окончания периода : ";
+    cin >> finish;
+
+    min = begin-1;
+    max = begin-1;
+
+    for (int i = begin; i <= finish-1; i++)
+    {
+        if (money[i] > money[max])
+        {
+            max = i;
+        }
+        if (money[i] < money[min])
+        {
+            min = i;
+        }
+
+    }
+    cout << "Максимальный доход был в месяце  " << max+1 << "\n";
+    cout << "Минимальный доход был в месяце  " << min+1 << "\n";
+}
---
+++ part2.2/part2.2.cpp
@@ -0,0 +1,99 @@
+﻿// part2.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+
+{
+
+
+    const int N=20;
+    int min , max,  a, b;
+    int sum = 0, proChet =1;
+    long pro=1;
+    int randomDigits[N] = {};
+    srand(time(NULL));
+
+    cout << "*****Диапазон случайных чисел будет от -а до b  ******* \n";
+
+    cout << "Введите начало диапазона a =  ";
+    cin >> a;
+    cout << "Введите конец  диапазона b =  ";
+    cin >> b;
+
+
+   
+    for (int i = 0; i < N; i++)
+    {
+        randomDigits[i] = -a + rand() % b;
+        cout << randomDigits[i] << "\n";
+
+        if (randomDigits[i] < 0)
+        {
+            sum += randomDigits[i];
+
+        }
+        if (i % 2 == 0)
+        {
+            proChet = proChet * randomDigits[i];
+        }
+    }
+   
+    min = 0;
+    max = 0;
+   
+    for (  int i = 0; i < N; i++)
+    {
+
+
+        if (randomDigits[i] < randomDigits[min])
+        {
+            min = i;
+            
+        }
+        if (randomDigits[i] > randomDigits[max])
+        {
+            
+            max = i;
+        }
+        
+
+    }
+    if (max < min)
+    {
+        for (int i = max + 1; i < min; i++)
+            pro = pro * randomDigits[i];
+    }
+    else if (min < max)
+    {
+        for (int i = min + 1; i < max; i++)
+            pro = pro * randomDigits[i];
+    }
+        
+   
+    int first, last, sumOtr=0;
+    for (first = 0; first < N; first++)
+    {
+        if (randomDigits[first] < 0) break;
+    }
+    for (last = N; last > first; last--)
+    {
+        if (randomDigits[last] < 0) break;
+    }
+    for (int i = first + 1; i < last; i++)
+    {
+        sumOtr += randomDigits[i];
+    }
+
+
+    cout << "\n" << "Сумма отрицательных чисел равна : " << sum << "\n";
+    
+    cout << "Произведение чисел между минимальным и максимальным числом = " << pro << "\n";
+
+    cout << "Произведение четных элементов массива = " << proChet << "\n";
+
+    cout << "Сумма между первым и последним отрицательным числом = " << sumOtr  << " \n";
+}
